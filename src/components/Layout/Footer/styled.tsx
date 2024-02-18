4c85a5e0f89391e7db89010535ca084800256066
404Nikhil <nicksd111@gmail.com>
2024-02-18 23:49:58 +0530

    fix2

--- src/components/Layout/Footer/styled.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from '@mui/material/styles';
-
-export const FooterDiv = styled('footer')`
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-`;
--- src/components/themes/ThemeToggler/styled.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles';
-import MuiIconButton from '@mui/material/IconButton';
-
-export const IconButton = styled(MuiIconButton)`
-  width: 42px;
-  height: 42px;
-  margin-left: 8px;
-`;
